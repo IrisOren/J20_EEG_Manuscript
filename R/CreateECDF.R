3f173244f85cae929d97412f8e4225b679854cfc
IrisOren <nerosiri@yahoo.co.uk>
2017-01-30 18:34:37 +0000

    Edited createECDF to apend genotype and animalID as factors to be used in plotting
    Need to tidy and comment

--- R/CreateECDF.R
+++ R/CreateECDF.R
@@ -1,4 +1,4 @@
-CreateECDF<-function(DF){
+CreateECDF<-function(DF, genotypeFactor, AnimalIDFactor){
 SpikeCount_df<-data.frame(SpikeCount=DF$SpikeCount)
 
 SpikeCount_df <- SpikeCount_df %>% group_by(SpikeCount) %>%  
@@ -7,6 +7,28 @@ SpikeCount_df <- SpikeCount_df %>% group_by(SpikeCount) %>%
 SpikeCount_df <- SpikeCount_df %>%  
   arrange(SpikeCount) %>% 
   mutate(CumFrequency=cumsum(NumIntervals))
+#And Normalise CumFrequency by max of CumFrequency
+  SpikeCount_df<- SpikeCount_df %>%
+    mutate(CumFrequency=CumFrequency/max(CumFrequency))
+
+#Add genotype
+if(genotypeFactor=="WT"){
+  SpikeCount_df <- SpikeCount_df %>% 
+    mutate(Genotype=1)
+}
+else{
+  SpikeCount_df <- SpikeCount_df %>% 
+    mutate(Genotype=2)
+}
+
+ 
+  SpikeCount_df$Genotype<-as.factor(SpikeCount_df$Genotype)
+  
+  #Add animalID
+  SpikeCount_df<- SpikeCount_df %>%
+    mutate(AnimalID=AnimalIDFactor)
+
+
 return(SpikeCount_df) 
 }
 
