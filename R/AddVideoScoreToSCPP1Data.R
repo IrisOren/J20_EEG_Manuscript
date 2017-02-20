d21361fbb28e5b5efdb8ed324415c8f750eee695
IrisOren <nerosiri@yahoo.co.uk>
2017-02-20 16:53:02 +0000

    Fixed bug in loops and function call of AddVideoScotreToSCPP1Data. Now correctly assigns S/W variable according to time specified in video scoring

--- R/AddVideoScoreToSCPP1Data.R
+++ R/AddVideoScoreToSCPP1Data.R
@@ -1,19 +1,37 @@
 #Function which writes the behavioural scoring variable from "VideoScore" to each row of SCPP1Time using VideoTime
 AddVideoScoreToSCPP1Data<-function(SCPP1Time, VideoTime, VideoScore){
-  j<-0
-  SCPP1Score<-vector(length=length(SCPP1Time))    #Initialise vector for video score
-  for(i in 1:(length(VideoTime)-1)){
+  j<-1
+  #SCPP1Score<-vector(length=length(SCPP1Time)) #Initialise vector for video score
+  SCPP1Score<-c()
+  SCPP1Score<-as.character(SCPP1Score)
+  i<-1
+  for(i in 1:length(VideoTime)){
     VideoScoreCurrent<-VideoScore[i]
-    
-    repeat{
-      SCPP1Score[j]<-VideoScoreCurrent
-      j<-j+1
-      if(SCPP1Time[j]>=VideoTime[i+1]){
-        break
+    if(i<length(VideoTime)){
+        while(SCPP1Time[j]<=VideoTime[i+1]){
+                SCPP1Score[j]<-VideoScoreCurrent
+                j<-j+1
+        }
+            # printout<-paste(SCPP1Time[j], VideoTime[i+1], sep="-")
+    } else{
+        repeat{
+          SCPP1Score[j]<-VideoScoreCurrent
+          j<-j+1
+        if(j==length(SCPP1Time)+1){
+          break
+        }
       }
-    }
+      }
+        
+      #repeat{
+     # SCPP1Score[j]<-VideoScoreCurrent
+    #  j<-j+1
+    #  printout<-paste(SCPP1Time[j], VideoTime[i+1], sep="-")
+    #  printout
+    #  if(SCPP1Time[j]>VideoTime[i+1]){
+     #   break
+      #}
+    #}
   }
-  
-  
-  return(SCPP1Score)
+return(SCPP1Score)
 }
