36516a9a65d8055917a171343f7f95af1d7594a1
IrisOren <nerosiri@yahoo.co.uk>
2017-02-21 11:36:16 +0000

    Deleted commented lines and added a comment

--- R/AddVideoScoreToSCPP1Data.R
+++ R/AddVideoScoreToSCPP1Data.R
@@ -7,7 +7,7 @@ AddVideoScoreToSCPP1Data<-function(SCPP1Time, VideoTime, VideoScore){
   i<-1
   for(i in 1:length(VideoTime)){
     VideoScoreCurrent<-VideoScore[i]
-    if(i<length(VideoTime)){
+    if(i<length(VideoTime)){ #condition and break needed to make sure that j!>length(VideoTime-1) so that while condition can be evaluated
         while(SCPP1Time[j]<=VideoTime[i+1]){
           if(j<length(SCPP1Time)){
                 SCPP1Score[j]<-VideoScoreCurrent
@@ -15,7 +15,7 @@ AddVideoScoreToSCPP1Data<-function(SCPP1Time, VideoTime, VideoScore){
           } else {
             break
             }
-            # printout<-paste(SCPP1Time[j], VideoTime[i+1], sep="-")
+    
         } 
     }else{
         repeat{
@@ -27,15 +27,7 @@ AddVideoScoreToSCPP1Data<-function(SCPP1Time, VideoTime, VideoScore){
       }
       }
         
-      #repeat{
-     # SCPP1Score[j]<-VideoScoreCurrent
-    #  j<-j+1
-    #  printout<-paste(SCPP1Time[j], VideoTime[i+1], sep="-")
-    #  printout
-    #  if(SCPP1Time[j]>VideoTime[i+1]){
-     #   break
-      #}
-    #}
+    
   }
 return(SCPP1Score)
 }
