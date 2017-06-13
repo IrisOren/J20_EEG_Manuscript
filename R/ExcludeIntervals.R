62622cbb582d220486d9e01283720a38050fc07c
IrisOren <nerosiri@yahoo.co.uk>
2017-06-13 15:05:26 +0100

    Amended for SCPP3 characterisics file with loss as a float replacing the epoch class

--- R/ExcludeIntervals.R
+++ R/ExcludeIntervals.R
@@ -1,15 +1,12 @@
 #Exclude intervals:
 # a)  classified as L
 # b) with theta and delta power approx 0 (ie. transmitters off)
-# c) Excessive delta power which we classify as artifacts. Factor is Factor*SDDelta for exclusion
-ExcludeIntervals <- function(DF, Factor){
+# c) Excessive delta power which we classify as artifacts. 
+ExcludeIntervals <- function(DF, DeltaPowerThreshold){
   DF <-filter(DF , EpochClass!="L")
   DF <- filter(DF, Theta>0.00001 & Delta>0.00001)
-  MeanDelta<-mean(DF$Delta)
-  sdDelta<-sd(DF$Delta)
-  DF <- filter(DF, Delta < MeanDelta+Factor*sdDelta)
-  DF <- filter(DF, Delta > MeanDelta-Factor*sdDelta)
-  return(DF)
+  DF <- filter(DF, Delta > DeltaPowerThreshold)
+return(DF)
   
   
   
