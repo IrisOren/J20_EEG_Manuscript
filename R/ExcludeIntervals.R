c0598a3a46f407de7cfe939ba3f80cdbae1a2419
IrisOren <nerosiri@yahoo.co.uk>
2017-06-13 16:13:40 +0100

    Changed Loss condition to Loss > LossThreshold

--- R/ExcludeIntervals.R
+++ R/ExcludeIntervals.R
@@ -1,9 +1,9 @@
 #Exclude intervals:
-# a)  classified as L
+# a)  with Loss > LossThreshold
 # b) with theta and delta power approx 0 (ie. transmitters off)
 # c) Excessive delta power which we classify as artifacts. 
-ExcludeIntervals <- function(DF, DeltaPowerThreshold){
-  DF <-filter(DF , EpochClass!="L")
+ExcludeIntervals <- function(DF, LossThreshold, DeltaPowerThreshold){
+  DF <-filter(DF , Loss > LossThreshold)
   DF <- filter(DF, Theta>0.00001 & Delta>0.00001)
   DF <- filter(DF, Delta > DeltaPowerThreshold)
 return(DF)
