5a1f5bb05a26b3d7a34236c746d6c27765611df1
IrisOren <nerosiri@yahoo.co.uk>
2017-06-15 17:19:52 +0100

    Updated ExcludeIntervals and ImportData

--- R/ExcludeIntervals.R
+++ R/ExcludeIntervals.R
@@ -1,15 +1,12 @@
 #Exclude intervals:
-# a)  classified as L
+# a)  with Loss > LossThreshold
 # b) with theta and delta power approx 0 (ie. transmitters off)
-# c) Excessive delta power which we classify as artifacts. Factor is Factor*SDDelta for exclusion
-ExcludeIntervals <- function(DF, Factor){
-  DF <-filter(DF , EpochClass!="L")
+# c) Excessive delta power which we classify as artifacts. 
+ExcludeIntervals <- function(DF, LossThreshold, DeltaPowerThreshold){
+  DF <-filter(DF , Loss > LossThreshold)
   DF <- filter(DF, Theta>0.00001 & Delta>0.00001)
-  MeanDelta<-mean(DF$Delta)
-  sdDelta<-sd(DF$Delta)
-  DF <- filter(DF, Delta < MeanDelta+Factor*sdDelta)
-  DF <- filter(DF, Delta > MeanDelta-Factor*sdDelta)
-  return(DF)
+  DF <- filter(DF, Delta > DeltaPowerThreshold)
+return(DF)
   
   
   
