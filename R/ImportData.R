e5d8a74ae875df0c8b901546c13138e6c8e2d486
IrisOren <nerosiri@yahoo.co.uk>
2017-06-20 15:37:20 +0100

    Amended for SCPP4V1

--- R/ImportData.R
+++ R/ImportData.R
@@ -5,20 +5,20 @@
 #OUTPUT: Returns dataframe of data from all output files
 #v1: 300716
 #v2: 130617: Amended for SCPP3 which has loss as a float instead of EpochClass
-
+#v3: 200617: Amended for SCPP4 to add burst count
 
 ImportData<-function(data_dir){
   #Lists all files of form SCPP1V1.txt
-  FileList<-list.files(path=data_dir, pattern="SCPP3V2.txt") #List of all files in working directory
+  FileList<-list.files(path=data_dir, pattern="SCPP4V1.txt") #List of all files in working directory
   EndFile<-length(FileList)  #This will be changed to length of FileList
   for (i in 1:EndFile){
     #Write to a temp dataframe
     path_and_file<-paste(data_dir, FileList[i], sep="/") #Use full path to file to allow for knitr to work
     DF_temp<-read.table(path_and_file, sep=" ")
     #Change variable names
-    names(DF_temp)<-c("FileName", "Seconds", "Chan", "Loss", "SpikeCount", "Delta", "Theta")
+    names(DF_temp)<-c("FileName", "Seconds", "Chan", "Loss", "SpikeCount", "BurstCount", "Delta", "Theta")
     #Remove columns with null data
-    DF_temp<-select(DF_temp, FileName, Seconds, Chan, Loss, SpikeCount, Delta, Theta) 
+    DF_temp<-select(DF_temp, FileName, Seconds, Chan, Loss, SpikeCount, BurstCount, Delta, Theta) 
     #Add current temp dataframe to combined SCPP_DF
      if(i==1){
       SCPP_DF<-DF_temp
